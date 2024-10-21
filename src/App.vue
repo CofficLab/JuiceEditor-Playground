b91ec8c10879402046b78e8885f6583d929172ee
Nookery <nooks@qq.com>
2024-10-21 16:31:00 +0800

    🎨 Chore:  Minor adjustments

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 import { defineJuiceEditor, EventName, EventType } from '@coffic/juice-editor'
 
 defineJuiceEditor()
 
-window.addEventListener(EventName, (event) => {
+window.addEventListener(EventName, ((event: CustomEvent) => {
   console.log(EventName, event)
 
   if (event.detail.type === EventType.editorReady) {
     let drawLink = "/drawio/webapp/index.html?"
-    console.log('editorReady, set draw link', drawLink)
-    window.api.config.setDrawIoLink(drawLink)
-    window.api.node.setHTML("<h1>Juice Editor</h1><p>Hello World</p>")
+    console.log('editorReady, set draw link', drawLink);
+    (window as any).api.config.setDrawIoLink(drawLink);
+    (window as any).api.node.setHTML("<h1 class='ml-0'>Juice Editor</h1><p class='ml-0'>Hello World</p>");
   }
-})
+}) as EventListener)
 </script>
 
 <template>
